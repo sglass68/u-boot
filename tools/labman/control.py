3c5ea78da29ee8e40c1ef76370a02a4ce533fbfc
Simon Glass <sjg@chromium.org>
2020-03-29 15:43:05 -0600

    WIP: labman proof-of-concept
    This contains a start on 'labman', a tool to manage your hardware lab. It
    works in conjunction with tbot and pytest to make it easier to set usp your
    lab and keep it working.
    
    You can use labman to:
    - define available boards and how they are connected. E.g. a LinkSprite
        pcduino3 may have an uSD connection to an SDwire (with USB connection
        to lab host), a UART connection to a USB UART dongle, a USB relay for
        reset and a USB YKUSH board for power.
    - generate a list of avalable boards
    - multithreadeed, dependency-based check that a board is working
        correctly, generating a list of things to do to get all boards working
        fully (but currently it doesn't test DUTs)
    - generate board config files for tbot
    - generate udev files
    - provision devices ready for use in the lab (currently only SDwire)
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    
    imx
    
    wip
    
    wip
    
    wip
    
    hid
    
    wip
    
    wip
    
    Change-Id: If208786d914b7df34a44413e804be23d0d6be449
    
    portserver
    
    Change-Id: I4687136e2884780903d702c93dacc5b89f75b11b
    
    wip
    
    Change-Id: Ie1b432dab5bc8e1f0629de65804443ec7f08b0d0
    
    wip
    
    Change-Id: I844b5af18f5c4cb0b9e0869c0884b7ddb5acdf25
    
    wip
    
    Change-Id: I43f111ec72a281269a77934bd632582b5f8f57c6
    
    wip
    
    Change-Id: Ia014e580dbf4822c109c21c528b0e04d079f344c
    
    wip
    
    Change-Id: I03ce2c92e631c018cfc14112f141007cb2ded723

---
+++ tools/labman/control.py
@@ -0,0 +1,40 @@
+# SPDX-License-Identifier: GPL-2.0+
+# Copyright 2020 Google LLC
+# Written by Simon Glass <sjg@chromium.org>
+#
+# Main control for labman
+#
+
+from lab import Lab
+
+# Used to access the lab for testing
+test_lab = None
+
+def Labman(args, lab=None):
+    if args.lab:
+        if not lab:
+            global test_lab
+            lab = Lab()
+            test_lab = lab
+        lab.read(args.lab)
+
+    if args.single_threaded:
+        lab.set_num_threads(0)
+
+    if args.remote:
+        lab.set_remote(args.remote)
+
+    lab.setup_state_dir()
+
+    if args.cmd == 'check':
+        lab.check(args.parts)
+    elif args.cmd == 'emit':
+        lab.emit(args.output_dir, args.dut, args.ftype)
+    elif args.cmd == 'ls':
+        lab.show(args.args)
+    elif args.cmd == 'prov':
+        lab.provision(args.component, args.name, args.serial)
+    elif args.cmd == 'start':
+        lab.start_daemons()
+    elif args.cmd == 'scan':
+        lab.scan()
